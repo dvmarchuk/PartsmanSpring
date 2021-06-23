b27865acfa4177c105e35ffee3c0a261ec0c756b
Dennis Marchuk <dvmarchuk4@gmail.com>
2021-06-23 13:40:39 +0300

    Initial commit

---
+++ src/main/java/com/example/partsmanspring/PartsmanSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.partsmanspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartsmanSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartsmanSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/partsmanspring/PartsmanSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.partsmanspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartsmanSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
